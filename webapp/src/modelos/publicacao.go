b82e78c7b271b8d99a18414b4597f27776c09d2e
Luiz Rosa <lcarlosrjr@gmail.com>
2022-05-24 15:42:56 -0300

    Publicacoes na pagina principal ao logar
    publicacoes do usuario e de quem ele segue na pagina principal ao logar/autenticar (home)

--- webapp/src/modelos/publicacao.go
+++ webapp/src/modelos/publicacao.go
@@ -2,6 +2,7 @@ package modelos
 
 import "time"
 
+// Publicacao representa publicacao feita pelo usuario
 type Publicacao struct {
 	ID        uint64    `json:"id,omitempty"`
 	Titulo    string    `json:"titulo,omitempty"`
